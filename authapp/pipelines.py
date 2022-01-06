07a2a11086494b519457afa703333485d3f04c1f
wwfirsov <wwfirsov@mail.ru>
2022-01-06 19:29:09 +0300

    dz2-3

--- authapp/pipelines.py
+++ authapp/pipelines.py
@@ -44,13 +44,13 @@ def save_user_profile(backend, user, response, *args, **kwargs):
 
     user.save()
 
-    if data['photo_200']:
-        photo_link = data['photo_200']
-        photo_response = requests.get(photo_link)
-        path_photo = f'user_image/{user.pk}.jpg'
-        with open(f'media/{path_photo}','wb') as photo:
-            photo.write(photo_response.content)
-        user.image = path_photo
+    # if data['photo_200']:
+    #     photo_link = data['photo_200']
+    #     photo_response = requests.get(photo_link)
+    #     path_photo = f'user_image/{user.pk}.jpg'
+    #     with open(f'media/{path_photo}','wb') as photo:
+    #         photo.write(photo_response.content)
+    #     user.image = path_photo
 
     if data['personal']['langs']:
         user.userprofile.langs = data['personal']['langs'][0] if len(data['personal']['langs'][0]) > 0 else 'EN'
