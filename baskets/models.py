367cd9a57fe9e056a677a4d7d29a60ec03a0b9df
wwfirsov <wwfirsov@mail.ru>
2022-01-23 13:45:29 +0300

    dz2-4

--- baskets/models.py
+++ baskets/models.py
@@ -5,9 +5,21 @@ from django.db import models
 from authapp.models import User
 from mainapp.models import Product
 
+class BasketQuerySet(models.QuerySet):
+    def delete(self, *args, **kwargs):
+        for item in self:
+            item.product.quantity += item.quantity
+            item.product.save()
+        super(BasketQuerySet, self).delete(*args, **kwargs)
 
 
 class Basket(models.Model):
+
+    objects = BasketQuerySet.as_manager()
+
+
+
+
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
@@ -32,3 +44,22 @@ class Basket(models.Model):
     def total_quantity(self):
         baskets = Basket.objects.filter(user=self.user)
         return sum(basket.quantity for basket in baskets)
+
+    # def delete(self, *args, **kwargs):
+    #     self.product.quantity += self.quantity
+    #     self.product.save()
+    #     super(Basket, self).delete(*args, **kwargs)
+    #
+    # def save(self, *args, **kwargs):
+    #     if self.pk:
+    #         get_item = self.get_item(int(self.pk))
+    #         self.product.quantity -= self.quantity - get_item
+    #     else:
+    #         self.product.quantity -= self.quantity
+    #
+    #     self.product.save()
+    #     super(Basket, self).save(*args, **kwargs)
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.get(pk=pk).quantity
